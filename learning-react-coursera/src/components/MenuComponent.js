565f8adaf311c60b121239a45681c65010120d14
MuhammadSaqib786 <saqisaqi.sk@gmail.com>
2022-10-24 14:59:35 +0500

    Using onChange and setState method and Card

--- learning-react-coursera/src/components/MenuComponent.js
+++ learning-react-coursera/src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Media } from "reactstrap";
+import { Card, CardBody, CardImg, CardImgOverlay, CardText, CardTitle } from "reactstrap";
 
 
 class Menu extends Component {
@@ -7,30 +7,53 @@ class Menu extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            sleectedDish: null
         }
       }
 
+      onDishSelect(dish) {
+        this.setState({ sleectedDish: dish })
+      }
+
+      renderDish(dish) {
+        if (dish != null)
+        {
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>
+                            {dish.anme}
+                        </CardTitle>
+                        <CardText>
+                            {dish.description}
+                        </CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else {
+            return(
+                <div>
+                    
+                </div>
+            );
+        }
+      }
     render() {
         
         const menu= this.props.dishes.map((dish) => {
             return (
-                <div key={dish.id} className='col-12 mt-5'>
-                    <div className="d-flex">
-            <div className="flex-shrink-0">
-              <Media object src={dish.image} alt={dish.name} >
+                <div key={dish.id} className='col-12 col-md-5 m-1'>
+                    <Card onClick={() => this.onDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>
+                                {dish.name}
+                            </CardTitle>
+                        </CardImgOverlay>
 
-              </Media>
-            </div>
-            <div className="flex-grow-1 ms-3">
-              <h2 className="mt-0">
-                {dish.name}
-                </h2>
-              <p>
-                {dish.description}
-                </p>
-            </div>
-                </div>
+                    </Card>
                 </div>
 
             );
@@ -38,10 +61,12 @@ class Menu extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
-                    
+    
+                        {menu}   
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.sleectedDish)}
+
                 </div>
             </div>
 
